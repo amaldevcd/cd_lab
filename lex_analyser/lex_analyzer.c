62eafce920053c4a33d56373f527b932e26d6e0a
amaldevcd <mailtoamaldevcd@gmail.com>
2023-11-20 19:32:57 +0530

    lexical analyser

---
+++ lex_analyser/lex_analyzer.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int isKeyword(char buffer[])
+{
+    char keywords[38][10] = {"auto", "break", "case", "char", "const", "continue", "default",
+                             "do", "double", "else", "enum", "extern", "float", "for", "goto",
+                             "if", "int", "long", "register", "return", "short", "signed",
+                             "sizeof", "static", "struct", "switch", "typedef", "union",
+                             "unsigned", "void", "volatile", "while", "scanf", "printf", "stdioh", "stdlibh", "main", "include"};
+    int i;
+    for (i = 0; i < 38; ++i)
+    {
+        if (strcmp(keywords[i], buffer) == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    char c, buffer[31], operators[] = "+-*/%=";
+    FILE *fp;
+    int i, j = 0;
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("Error while opening the file\n");
+        exit(0);
+    }
+
+    while ((c = fgetc(fp)) != EOF)
+    {
+        for (i = 0; i < 6; ++i)
+        {
+            if (c == operators[i])
+                printf("%c is operator\n", c);
+        }
+         
+        if (isdigit(c))
+        {
+        printf("%c is a digit\n", c);
+        }
+        
+        else if (isalpha(c))
+        {
+            buffer[j++] = c;
+        }
+        else if ((c == ' ' || c == '\t' || c == '\n' || c=='(') && (j != 0))
+        {
+            buffer[j] = '\0';
+            j = 0;
+
+            if (isKeyword(buffer) == 1)
+                printf("%s is keyword\n", buffer);
+            else
+                printf("%s is identifier\n", buffer);
+        }
+    }
+    fclose(fp);
+    return 0;
+}
